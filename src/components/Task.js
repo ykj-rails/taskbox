e9d2e45e8f140081b32924de60996681dd6eb261
ykj-rails <yukiji.0307@gmail.com>
2022-07-25 08:49:28 +0900

    [update] Addons

--- src/components/Task.js
+++ src/components/Task.js
@@ -30,7 +30,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ background: 'red' }}
+          style={{ textOverflow: 'ellipsis' }}
         />
       </label>
 
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -33,3 +33,12 @@ Archived.args = {
     state: 'TASK_ARCHIVED',
   },
 }
+
+export const LongTitle = Template.bind({})
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title:
+      'Controls is a great way to get non-developers playing with your components and stories. It can do much more than we ve seen here; we recommend reading the official documentation to learn more about it. However, there are many more ways you can customize Storybook to fit your workflow with addons. In the create an addon guide well teach you that, by creating an addon that will help you supercharge your development workflow.',
+  },
+}
